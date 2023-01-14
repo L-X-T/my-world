3a6e9874fb0148f14683dfbb272d8d728e5d0c7d
Alex Thalhammer <alex@angular-software.at>
2023-01-14 11:13:46 +0100

    Updating Angular to latest v15 and playing around

--- src/app/flight-booking/flight-search/flight.service.ts
+++ src/app/flight-booking/flight-search/flight.service.ts
@@ -8,20 +8,18 @@ import { Flight } from '../../entities/flight';
 @Injectable({ providedIn: 'root' })
 export class FlightService {
   url = 'http://www.angular.at/api/flight';
+  headers = new HttpHeaders().set('Accept', 'application/json');
   // url = 'https://demo.angulararchitects.io/api/Flight';
 
   constructor(private http: HttpClient) {}
 
   find(from: string, to: string): Observable<Flight[]> {
-    const headers = new HttpHeaders().set('Accept', 'application/json');
     const params = new HttpParams().set('from', from).set('to', to);
 
-    return this.http.get<Flight[]>(this.url, { headers, params });
+    return this.http.get<Flight[]>(this.url, { headers: this.headers, params });
   }
 
   save(flight: Flight): Observable<Flight> {
-    const headers = new HttpHeaders().set('Accept', 'application/json');
-
-    return this.http.post<Flight>(this.url, flight, { headers });
+    return this.http.post<Flight>(this.url, flight, { headers: this.headers });
   }
 }

L-X-T/sgbsrv-1

[RF] Parametrizable Validators

src/app/flight-booking/shared/validation/city-validator.ts
@@ -1,15 +1,17 @@
-import { AbstractControl } from '@angular/forms';
+import { AbstractControl, ValidatorFn } from '@angular/forms';
 
-export function validateCity(c: AbstractControl) {
-  const validCities: string[] = ['Graz', 'Wien', 'Hamburg', 'Berlin'];
-  if (c.value && validCities.indexOf(c.value) === -1) {
-    return {
-      city: {
-        actualValue: c.value,
-        validCities: validCities
-      }
-    };
-  }
+export function validateCity(validCities: string[]): ValidatorFn {
+  // const validCities: string[] = ['Graz', 'Wien', 'Hamburg', 'Berlin'];
+  return (c: AbstractControl) => {
+    if (c.value && validCities.indexOf(c.value) === -1) {
+      return {
+        city: {
+          actualValue: c.value,
+          validCities: validCities
+        }
+      };
+    }
 
-  return null;
+    return null;
+  };
 }

L-X-T/my-world

(cherry picked from commit 22c52295ad7cf5226f9475b808b3021e89d57332)

src/app/flight-booking/shared/validation/city-validator.ts
@@ -1,15 +1,17 @@
-import { AbstractControl, ValidationErrors } from '@angular/forms';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
-export function validateCity(c: AbstractControl): ValidationErrors | null {
-  const validCities: string[] = ['Graz', 'Wien', 'Hamburg', 'Berlin'];
-  if (c.value && validCities.indexOf(c.value) === -1) {
-    return {
-      city: {
-        actualCity: c.value,
-        validCities: validCities
-      }
-    };
-  }
+export function validateCity(validCities: string[]): ValidatorFn {
+  // const validCities: string[] = ['Graz', 'Wien', 'Hamburg', 'Berlin'];
+  return (c: AbstractControl): ValidationErrors | null => {
+    if (c.value && validCities.indexOf(c.value) === -1) {
+      return {
+        city: {
+          actualCity: c.value,
+          validCities: validCities
+        }
+      };
+    }
 
-  return null;
+    return null;
+  };
 }